f6e9181b9ab8f2960f09d330b63288f6534bc382
Haibao Tang <tanghaibao@gmail.com>
2026-09-25 13:24:59 -0700

    Step 6: prepare_design_matrix

--- model.py
+++ model.py
@@ -63,8 +63,12 @@ def add_bias_column(X):
     n = X.shape[0]
     return np.column_stack((np.ones(n), X))
 
-# Step 6 - prepare_design_matrix (not yet solved)
-# TODO: implement
+# Step 6 - prepare_design_matrix
+def prepare_design_matrix(X, mean, std):
+    # TODO: Standardize features then add the bias column to form the design matrix.
+    X = (X - mean) / std
+    n = X.shape[0]
+    return np.column_stack((np.ones(n), X))
 
 # Step 7 - predict_linear (not yet solved)
 # TODO: implement
