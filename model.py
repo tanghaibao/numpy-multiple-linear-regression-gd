ad1e9115c669d5b3970280caf407f7f9bd8d0c84
Haibao Tang <tanghaibao@gmail.com>
2026-09-25 13:59:05 -0700

    Step 8: mse_loss

--- model.py
+++ model.py
@@ -84,8 +84,10 @@ def predict_linear(X, weights):
     # TODO: Return the predicted target vector from X and weights
     return X @ weights
 
-# Step 8 - mse_loss (not yet solved)
-# TODO: implement
+# Step 8 - mse_loss
+def mse_loss(y_true, y_pred):
+    # TODO: Return the average of squared residuals as a scalar float.
+    return ((y_true - y_pred) ** 2).mean()
 
 # Step 9 - mse_gradient (not yet solved)
 # TODO: implement
